a94800cf7d1248cbcb9b343fd1a5264b8682b097
moise <moisemlg90@gmail.com>
2024-02-16 19:57:45 +0200

    mark task done

--- src/hooks/use-add-transactions.js
+++ src/hooks/use-add-transactions.js
@@ -34,7 +34,6 @@ export const useAddTransaction = () => {
 
 /* 
 
-DM: todoMM: I forgot to put in the other repo: I want you to take 2 hours to review your Microverse notes on ruby, specifically the database schema design and the SQL queries. Then, I want you to write a schema (i.e., database tables for users, messages, etc) that would work for your real-time-chat app if it were using a ruby back end instead of firestore. Then write teh SQL queries that would be necessary to get the same data that you are getting from firestore in your real-time-chat app.
-
+(done)DM: todoMM: I forgot to put in the other repo: I want you to take 2 hours to review your Microverse notes on ruby, specifically the database schema design and the SQL queries. Then, I want you to write a schema (i.e., database tables for users, messages, etc) that would work for your real-time-chat app if it were using a ruby back end instead of firestore. Then write teh SQL queries that would be necessary to get the same data that you are getting from firestore in your real-time-chat app.
 
 */
