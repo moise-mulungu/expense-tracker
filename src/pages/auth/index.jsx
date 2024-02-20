19f6ad55cf95af9d159b24bc3c9e4e83dc2dea73
moise <moisemlg90@gmail.com>
2024-02-20 17:31:25 +0200

    login authentication

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,10 +1,13 @@
 import { auth, googleAuthProvider } from '../../config/firebase-config'
 import { signInWithPopup } from 'firebase/auth'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Navigate } from 'react-router-dom'
 import '../../App.css'
+import { useGetUserInfo } from '../../hooks/use-get-user-info'
 
 export const Auth = () => {
   const navigate = useNavigate()
+  const {isAuth} = useGetUserInfo()
+
   const SignInWithGoogle = async () => {
     const results = await signInWithPopup(auth, googleAuthProvider)
     console.log(results)
@@ -20,6 +23,16 @@ export const Auth = () => {
     navigate('/expense-tracker')
   }
 
+  if (isAuth) {
+    return <Navigate to="/expense-tracker" />
+  } 
+
+  // or you can use the useEffect hook to check if the user is authenticated:
+  // useEffect(() => {
+  //   if (isAuth) {
+  //     navigate('/expense-tracker')
+  //   }
+
   return (
     <div className="login-page">
       <p>Sign In With Google to Continue</p>
@@ -30,3 +43,14 @@ export const Auth = () => {
   )
 }
 // Path: src/pages/auth/index.jsx
+
+/*
+To fix the regular login, i did the following:
+  1. imported the Navigate component from react-router-dom
+  2. imported the useGetUserInfo hook from the use-get-user-info file
+  3. created a variable called isAuth and set it equal to the isAuth property from the useGetUserInfo hook
+  4. created a conditional statement that checks if the user is authenticated and if they are, it will redirect them to the expense-tracker page
+  5. there is an alternative way to redirect the user to the expense-tracker page using the useEffect hook
+  6. the useEffect hook will check if the user is authenticated and if they are, it will redirect them to the expense-tracker page
+  7. tested the code and it works as expected.
+*/
